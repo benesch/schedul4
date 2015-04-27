1e6710601bba5213f55fdd34b7f5e8cc1ca616e4
Nikhil Benesch <nikhil.benesch@gmail.com>
2015-04-27 06:46:43 -0400

    tune guzzle for userspace scheduler

--- apps/ipcbench/src/guzzle.c
+++ apps/ipcbench/src/guzzle.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-#define NITERATIONS  (1 << 25)
+#define NITERATIONS     (1 << 30)
+#define PRINT_INTERVAL  (1 << 20)
 
 int run(void) {
   for (volatile int i = 0; i < NITERATIONS; i++) {
-    if ((i % 1000000) == 0) {
-      printf("GUZZLE\n");
+    if ((i % PRINT_INTERVAL) == 0) {
+      printf("GUZZLE %d\n", i / PRINT_INTERVAL);
     }
   }
+  printf("GUZZLE DONE\n");
 
   return 0;
 }
