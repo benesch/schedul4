d0ca2c42988a62b66e7ac73271ccd7ae1fb7d8bb
Nikhil Benesch <nikhil.benesch@gmail.com>
2015-05-15 05:34:58 -0400

    fix cycle counter tracker overflow

--- apps/ipcbench/include/ipcbench.h
+++ apps/ipcbench/include/ipcbench.h
@@ -3,7 +3,7 @@
  * types start with an uppercase letter. So we typedef an unsigned
  * integer appropriately.
  */
-typedef unsigned int UnsignedInteger;
+typedef unsigned long long UnsignedInteger;
 
 #define NTRIALS             1000
 #define TRIAL_SPOT_INTERVAL 333
